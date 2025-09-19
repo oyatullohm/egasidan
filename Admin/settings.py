2d0c17079b6b7c9bb5846e8c493c6ce0fcc9dde0
oyatullohm <marziyahoni@gmail.com>
2025-09-19 06:17:42 +0500

    f

--- Admin/settings.py
+++ Admin/settings.py
@@ -216,8 +216,8 @@ STATIC_URL = '/static/'
 
 
 STATIC_ROOT = os.path.join('static')
-MEDIA_ROOT = "/var/www/egasidan/media"
-MEDIA_URL = "/media/"
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = (
      os.path.join(BASE_DIR / "staticfiles"),
