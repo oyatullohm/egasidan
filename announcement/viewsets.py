b5953a0f145d95131573f131c6abb48b413f9cf2
oyatullohm <marziyahoni@gmail.com>
2025-09-24 21:47:11 +0500

    f

--- announcement/viewsets.py
+++ announcement/viewsets.py
@@ -3,6 +3,7 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework import viewsets
+from django.db.models import F
 from rest_framework import status
 from .serializers import *
 from .decorators import *
